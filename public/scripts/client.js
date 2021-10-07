3578b0d9eb339d3d8c7c33c69ab30557f8102e1e
KurtSpiker <kurt.spiker@gmail.com>
2021-10-07 07:08:30 +0000

    Loading tweets function working on inital page load, and when adding a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -36,39 +36,26 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  const data = [
-    {
-    "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-    },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-    },
-    {
-    "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-    "content": {
-        "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-    }
-  ]
+  
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      type: 'get',
+      success: function(data) {
+        renderTweets(data)
+      }
+    })
+  }
+  
   const renderTweets = function(tweets) {
-    // loops through tweets
     for (const tweet of tweets) {
-        $("#tweets-container").append(createTweetElement(tweet))
+        $("#tweets-container").prepend(createTweetElement(tweet))
     }
   }
-  renderTweets(data);
 
-  $("#new-tweet-form" ).submit(function(event) {
+  $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
-    $.post("/tweets", $(this).serialize()) 
+    $.post("/tweets", $(this).serialize(), loadTweets) 
   });
+  loadTweets();
 });
